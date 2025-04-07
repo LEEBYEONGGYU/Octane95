16f9dfbaa9b205d177c45167bef843226baf11cb
LEEBYEONGGYU <55679098+LEEBYEONGGYU@users.noreply.github.com>
2025-04-07 17:00:13 +0900

    기본세팅
    기본세팅

---
+++ android/app/src/main/kotlin/com/octane/octane95/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.octane.octane95
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await InAppWebViewController.setWebContentsDebuggingEnabled(true);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final String initialUrl = "http://gyudrive.ipdisk.co.kr/Untitled-1.html";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '옥탄가 계산기',
+      home: Scaffold(
+        body: SafeArea(
+          child: InAppWebView(
+            initialUrlRequest:  URLRequest(
+                url: WebUri.uri(Uri.parse(initialUrl!))),
+            onWebViewCreated: (controller) {
+              print("WebView created");
+            },
+            onLoadStop: (controller, url) {
+              print("Loaded: $url");
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
